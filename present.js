d5f7c3d27d06a438d10b8ad4682f1f4dccc73b7e
Huankento <116175143+Huankento@users.noreply.github.com>
2022-10-19 21:55:04 +0700

    Add files via upload

---
+++ present.js
@@ -0,0 +1,46 @@
+
+var to = 'Người iu của tui';
+var gift_url = '7138488927536892952.mp4';
+var gift_image_url = 'z3813003982781_b93fc12a9485e93275a23cdd333cef79.jpg';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show')
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
